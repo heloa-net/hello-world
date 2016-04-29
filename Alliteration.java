ccc7a7366ddc5bcf84e6f41824c3af3b7c06e766
Heloá Meneses <heloa@usp.br>
2016-04-29 12:50:18 -0300

    Alliteration problem from ACH2001 class
    Today's class
    URI: 1263 / 1253 / 1239

---
+++ Alliteration.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+
+public class Alliteration {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Digite uma frase");
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		while (entrada.hasNextLine()) { // Scanner de todo o texto
+			Scanner entrada2 = new Scanner(entrada.nextLine()); // Scanner de cada linha
+			while (entrada2.hasNext()) {
+				
+				char[]caracteres = new char[100];
+				String s = entrada2.next();
+				// System.out.println(s); Teste
+				for () {
+					
+					
+				}
+				
+			}
+			
+		}
+		
+		
+	}
+
+} 
