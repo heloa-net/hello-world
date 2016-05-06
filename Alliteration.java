c317f32a4d5de9b5d03141d0c6c7ff685565fe64
Heloá Meneses <heloa@usp.br>
2016-05-06 15:18:04 +0000

    Update Alliteration.java

--- Alliteration.java
+++ Alliteration.java
@@ -9,22 +9,36 @@ public class Alliteration {
 		Scanner entrada = new Scanner(System.in);
 		
 		while (entrada.hasNextLine()) { // Scanner de todo o texto
-			Scanner entrada2 = new Scanner(entrada.nextLine()); // Scanner de cada linha
-			while (entrada2.hasNext()) {
-				
-				char[]caracteres = new char[100];
-				String s = entrada2.next();
-				// System.out.println(s); Teste
-				for () {
-					
-					
-				}
-				
-			}
-			
-		}
-		
-		
-	}
+			Scanner entrada2 = new Scanner(entrada.nextLine().toLowerCase()); // Scanner de cada linha
 
+			while (entrada2.hasNext()) {
+				System.out.println(entrada2.nextLine());
+				String[] palavras;
+				palavras = entrada2.nextLine().split(" ");
+				int contador = 0;
+				
+				for (int i = 0; i < palavras.length; i++) {
+
+					char anterior = palavras[i].charAt(0);
+					char atual = palavras[i+1].charAt(0);
+					char proximo;
+
+					if (anterior == atual) {
+						contador++;
+						for (int k = i+2; k < palavras.length-1; k++) {
+							proximo = palavras[k].charAt(0);
+							if (anterior != proximo) {
+								i = k;
+								break;								
+							}
+						}		
+					}				
+  				     
+			System.out.println(contador);
+                }
+			}
+		}
+	}
 } 
+
+
